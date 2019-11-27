2143288f8076e740d6786fe0d4052a94e5cad8a1
jirwincline <jakobinwins@gmail.com>
2019-11-27 11:25:04 -0800

    state is passed into pathways

--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -1,48 +1,51 @@
 import React, { Component } from 'react'
-
+import { Link } from "react-router-dom";
 
 export default class Navbar2 extends Component {
     render() {
         return (
           <div /*class="collapse navbar-collapse"*/ id="product-navbar">
-
             <ul className="product-nav-ul">
               {/* <li id="LI_13">
                 <a href="/" id="A_14">
                   Home
                 </a>
               </li> */}
-              <li id="nav-item">
-                <a
-                  href="https://radhippie.com/t/httpradhippiecomttops"
-                  id="A_16"
-                >
-                  Paintings
-                </a>
-              </li>
-              <li id="nav-item">
-                <a href="https://radhippie.com/t/band-products" id="A_18">
-                  Jewels
-                </a>
-              </li>
-              <li id="nav-item">
-                <a href="https://radhippie.com/t/accessories" id="A_20">
-                  Cleansing
-                </a>
-              </li>
-              <li id="nav-item">
-                <a href="https://radhippie.com/t/backpacks" id="A_22">
-                  Retreats
-                </a>
-              </li>
-              <li id="nav-item tarot">
-                <a
-                  href="https://radhippie.com/t/tapestries-groovy-stuff"
-                  id="A_24"
-                >
-                  Tarot Readings
-                </a>
-              </li>
+              <Link to={{
+                pathname: '/products',
+                state: {
+                pageCategory: 'painting'
+              }
+              }}>
+                <li id="nav-item">Paintings</li>
+              </Link>
+              <Link to={{
+                pathname: '/products',
+                state: {
+                pageCategory: 'jewelry'
+              }
+              }}>
+                <li id="nav-item">Jewels</li>
+              </Link>
+              <Link to={{
+                pathname: '/products',
+                state: {
+                pageCategory: 'cleansing'
+              }
+              }}>
+                <li id="nav-item">Cleansing</li>
+              </Link>
+              <Link to={{
+                pathname: '/products',
+                state: {
+                pageCategory: 'retreats'
+              }
+              }}>
+                <li id="nav-item">Retreats</li>
+              </Link>
+              <Link to="/tarot">
+                <li id="nav-item">Tarot Readings</li>
+              </Link>
             </ul>
           </div>
         );

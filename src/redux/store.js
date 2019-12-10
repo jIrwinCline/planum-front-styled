ef6e2e48f2d4968e9fda3570d892c83169b1318d
jirwincline <jakobinwins@gmail.com>
2019-12-09 23:41:11 -0800

    initialized firebase and deployed as build

--- src/redux/store.js
+++ src/redux/store.js
@@ -28,8 +28,9 @@ const store = createStore(
   // )
   // NEW FIX:
   compose(
-    applyMiddleware( ...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware( ...middleware) 
+    // window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    //     window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
